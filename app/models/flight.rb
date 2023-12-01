b6deda80dd89da7bc33cac1dc514550c94925732
willofD13 <leonidastsiakas13@gmail.com>
2023-12-01 21:59:33 +0200

    Filter flights based on whether a form is submitted

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,13 @@
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
+
+    def self.search(search)
+        if search
+            self.where(departure_airport_id: search)
+        else
+            Flight.all
+        end
+    end
+
 end
