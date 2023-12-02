f151755db749d89665f145bfae792be4af3dd5dd
willofD13 <leonidastsiakas13@gmail.com>
2023-12-02 18:04:08 +0200

    Add rest of flight attributes

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,10 +1,11 @@
+require 'date'
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
 
-    def self.search(search)
-        if search
-            self.where(departure_airport_id: search)
+    def self.search(param1,param2,param3)
+        if param1
+            self.where(departure_airport_id: param1, arrival_airport_id: param2,start_datetime: param3)
         else
             Flight.all
         end
