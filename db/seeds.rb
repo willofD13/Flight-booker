06ec8aeaff8119381e128f036a74ee546d2f6e66
willofD13 <leonidastsiakas13@gmail.com>
2023-12-02 20:01:36 +0200

    Retrieve a morning and afternoon time from each day

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,28 +31,39 @@ Airport.delete_all
    airport_codes = Airport.all.map {|a| a.code }
    airport_pairs = airport_codes.permutation(2).to_a
 
-   (today..month).each do |d|
-      airport_finder(airport_pairs,today,month)
-   end
 
-   def airport_finder(airports,time1,time2)
+   def airport_finder(airports,day)
     airports.each do |a|
       a1 = Airport.find_by(code: a[0])
       a2 = Airport.find_by(code: a[1])
+      morning = morning_time(day)
+      afternoon = afternoon_time(day)
 
-      flight_creator(a1,a2,time1,time2)
-      flight_creator(a1,a2,time1,time2)
+      flight_creator(a1,a2,morning,afternoon)
+      flight_creator(a1,a2,morning,afternoon)
     end
    end
   
-   def flight_creator(departure_airport,arrival_airport,time1,time2)
+   def flight_creator(departure_airport,arrival_airport,morning,afternoon)
       Flight.create!({
-      start_datetime: Time.at((time2.to_f - time1.to_f)*rand + time1.to_f),
+      start_datetime: Time.at((afternoon.to_f - morning.to_f)*rand + morning.to_f),
       duration: "#{((1..24).to_a).sample} hours",
       departure_airport_id: departure_airport.id,
       arrival_airport_id: arrival_airport.id
       })
    end
+
+   def morning_time(time)
+    Faker::Time.between_dates(from: time, to: time, period: :morning)
+   end
+
+   def afternoon_time(time)
+    Faker::Time.between_dates(from: time, to: time, period: :afternoon)
+   end
+
+   (today..month).each do |d|
+    airport_finder(airport_pairs,d)
+ end
 
    
 p "created #{Flight.count} flights"
