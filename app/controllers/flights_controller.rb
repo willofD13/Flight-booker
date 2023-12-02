4d6f769d1d2bad6fba24b8378ea5171a5c88f282
willofD13 <leonidastsiakas13@gmail.com>
2023-12-02 18:02:51 +0200

    Provide inputs for passengers and date menus

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,8 @@
 class FlightsController < ApplicationController
     def index
-        @flights = Flight.search(params[:departure_airport_id])
+        @flights = Flight.search(params[:departure_airport_id],params[:arrival_airport_id],params[:start_datetime])
         @airport_options = Airport.all.map {|a| [a.code,a.id]}
+        @number_options = [[1],[2],[3],[4]]
+        @date_options = Flight.all.map {|f| [f.start_datetime.strftime('%Y-%m-%d'),f.start_datetime.strftime('%Y-%m-%d %H:%M:%S.%N')]}
     end
 end
