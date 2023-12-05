0f89cffae8553c3e4f255ed09c24a56b5e334f23
willofD13 <leonidastsiakas13@gmail.com>
2023-12-05 14:32:30 +0200

    Create a nested array with flight dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,6 @@ class FlightsController < ApplicationController
         @flights = Flight.search(params[:departure_airport_id],params[:arrival_airport_id],params[:date])
         @airport_options = Airport.all.map {|a| [a.code,a.id]}
         @number_options = [[1],[2],[3],[4]]
+        @dates_options = (Date.today..Date.today+20).map {|date| [date]}
     end
 end
