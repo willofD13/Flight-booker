fbe194b2a714d5ecd7f6f3aacb9204932f09f130
willofD13 <leonidastsiakas13@gmail.com>
2023-12-05 14:05:59 +0200

    Delete date array an rename flight date param

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,7 @@
 class FlightsController < ApplicationController
     def index
-        @flights = Flight.search(params[:departure_airport_id],params[:arrival_airport_id],params[:start_datetime])
+        @flights = Flight.search(params[:departure_airport_id],params[:arrival_airport_id],params[:date])
         @airport_options = Airport.all.map {|a| [a.code,a.id]}
         @number_options = [[1],[2],[3],[4]]
-        @date_options = Flight.all.map {|f| [f.start_datetime.strftime('%Y-%m-%d'),f.start_datetime.strftime('%Y-%m-%d %H:%M:%S.%N')]}
     end
 end
