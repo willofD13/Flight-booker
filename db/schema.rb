131b95a6af7dee532c319774c78b885a2c8cbbb4
willofD13 <leonidastsiakas13@gmail.com>
2023-12-05 17:07:59 +0200

    Create a booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_134938) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_145634) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
