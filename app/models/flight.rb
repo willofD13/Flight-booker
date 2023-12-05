c6967556c246abfb899b926946c27790c6862611
willofD13 <leonidastsiakas13@gmail.com>
2023-12-05 14:27:17 +0200

    Fix condition coding

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,7 +5,8 @@ class Flight < ApplicationRecord
 
     def self.search(param1,param2,param3)
         if param1
-            self.where(departure_airport_id: param1, arrival_airport_id: param2,date: param3)
+            self.where(departure_airport_id: param1, arrival_airport_id: param2, date: param3)
+        else
             Flight.all
         end
     end
