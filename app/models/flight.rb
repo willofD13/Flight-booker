323737de4f0d81e34913b720b841711b531abfcd
willofD13 <leonidastsiakas13@gmail.com>
2023-12-06 15:31:47 +0200

    Set up associations between Booking,Flight,Passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,7 @@ require 'date'
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
+    has_many :bookings
 
     def self.search(param1,param2,param3)
         if param1
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,5 @@
 class Passenger < ApplicationRecord
+    belongs_to :booking
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 end
