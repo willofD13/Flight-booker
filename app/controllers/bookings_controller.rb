393b4364586f6520d9f8a64b7d2c11676612a636
willofD13 <leonidastsiakas13@gmail.com>
2023-12-06 18:44:38 +0200

    Create blank passenger objects

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,8 @@ class BookingsController < ApplicationController
         @flight = Flight.find_by(time: params[:time],date: params[:date],departure_airport_id: params[:departure_airport],
         arrival_airport_id: params[:arrival_airport],duration: params[:duration])
         @flight.number_of_passengers = params[:number_of_passengers]
+        @flight.number_of_passengers.times do
+            @booking.passengers.build
+        end
     end
 end
