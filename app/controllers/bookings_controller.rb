fec14c0abaf93907dfb96331cd2daabe70482293
willofD13 <leonidastsiakas13@gmail.com>
2023-12-06 19:08:21 +0200

    Find the flight by its id

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,7 @@
 class BookingsController < ApplicationController
     def new
         @booking = Booking.new
-        @flight = Flight.find_by(time: params[:time],date: params[:date],departure_airport_id: params[:departure_airport],
-        arrival_airport_id: params[:arrival_airport],duration: params[:duration])
+        @flight = Flight.find_by(id: params[:flight_id])
         @flight.number_of_passengers = params[:number_of_passengers]
         @flight.number_of_passengers.times do
             @booking.passengers.build
