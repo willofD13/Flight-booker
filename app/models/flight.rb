6221b78153b71ae39c37b8c83753d7b14bee86db
willofD13 <leonidastsiakas13@gmail.com>
2023-12-07 14:47:20 +0200

    Destroy associated data

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,7 @@ require 'date'
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
-    has_many :bookings
+    has_many :bookings, dependent: :destroy
 
     def self.search(param1,param2,param3)
         if param1
