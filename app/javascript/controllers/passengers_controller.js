92432a75e02b5c3293561f77bf3e703ffc4eb1a0
willofD13 <leonidastsiakas13@gmail.com>
2023-12-23 19:51:52 +0200

    Create an action for creating an extra passenger form

--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static values= { number: Number }
+    static targets = ["template","form"]
+    
+
+    renderForm() {
+        let clone = this.templateTarget.content.cloneNode(true)
+        this.formTarget.appendChild(clone)
+    }
+
 }
