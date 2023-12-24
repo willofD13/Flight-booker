888efaccefee7447964caed146f829abcde592ec
willofD13 <leonidastsiakas13@gmail.com>
2023-12-24 13:21:26 +0200

    Declare number value

--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["template","form"]
-    
+    static values = { number: Number}
 
     renderForm() {
         let clone = this.templateTarget.content.cloneNode(true)
