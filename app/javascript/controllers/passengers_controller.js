62e41c6d99b4082b588f0f9276ccd381be4b5574
willofD13 <leonidastsiakas13@gmail.com>
2023-12-24 20:44:00 +0200

    Clone the last child node each time the button is clicked

--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -2,11 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["template","form"]
-    static values = { number: Number}
 
     renderForm() {
-        let clone = this.templateTarget.content.cloneNode(true)
-        this.formTarget.appendChild(clone)
+    
+        if (formTarget.hasChildNodes()) {
+            let clone = this.formTarget.lastChild.content.cloneNode(true)
+            this.formTarget.appendChild(clone)
+        } else {
+            let clone = this.templateTarget.content.cloneNode(true)
+            this.formTarget.appendChild(clone)
+        }
     }
 
     deleteForm() {
