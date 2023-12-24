29848729a320fe4c3e8a2640eabd3bc943049e48
willofD13 <leonidastsiakas13@gmail.com>
2023-12-24 16:08:48 +0200

    Add allow destroy option

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
     belongs_to :flight 
     has_many :passengers, dependent: :destroy
-    accepts_nested_attributes_for :passengers
+    accepts_nested_attributes_for :passengers, allow_destroy: true
 
     def as_json(options={})
         {:id => self.id,
