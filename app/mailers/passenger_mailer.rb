b90e105568f23a3554157c6ffe04b4db4aaab380
willofD13 <leonidastsiakas13@gmail.com>
2023-12-27 15:10:01 +0200

    Create an initial mail method

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'notifications@example.com'
+
+  def welcome_email
+    @passenger = 
+    mail(to: @passenger.email, subject: 'Welcome to My Awesome Site')
+  end
 end
