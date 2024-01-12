fd602bea7d35ac62e91f6e384ea6ac5d4fb56133
willofD13 <leonidastsiakas13@gmail.com>
2024-01-12 16:32:04 +0200

    Set delivery errors option to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -62,8 +62,21 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587 ,
+    domain: 'localhost:3000',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'leonidastsiakas13@gmail.com',
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
