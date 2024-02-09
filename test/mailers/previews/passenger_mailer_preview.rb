5e8834a66b20a8195ad53bfe03e95a93f7ef4d50
willofD13 <leonidastsiakas13@gmail.com>
2024-02-09 22:01:00 +0200

    Create mail preview

--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
 class PassengerMailerPreview < ActionMailer::Preview
-
+    def welcome_email
+        PassengerMailer.with(passenger: Passenger.first).welcome_email
+    end
 end
